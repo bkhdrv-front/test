8bf093070afe780a1b0ae647d68a438b0e55ce5d
Akbar <akbarbakh2000@gmail.com>
2020-04-08 23:40:56 +0500

    это наш первый коммит

---
+++ script.js
@@ -0,0 +1,14 @@
+// alert("Hello World");
+// console.log(alert);
+
+// confirm("Are u Idiot?");
+
+/*let answer = prompt("Write number on the picture", "265");
+console.log(typeof(answer));
+
+var lol = ['123'];
+console.log(lol);*/
+
+var pic = ['apple.bmp','orange.png','blackberry'];
+
+console.log(pic["2"]);
